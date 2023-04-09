e298b47165ce4822e3875f2fdb2d1e95f25e3360
DeveshPatel-14 <devpatel1130@gmail.com>
2023-04-09 18:44:29 +0530

    Test API commit

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+
+const UserSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required: true,
+            min: 2,
+            max: 100,
+        },
+        email:{
+            type:String,
+            required: true,
+            max: 50,
+            unique: true,
+        },
+        password:{
+            type: String,
+            required: true,
+            min: 5,
+        },
+        phoneNumber: String,
+        role:{
+            type: String,
+            enum: ["student", "admin", "superadmin"],
+            default:"admin",
+        },
+    },
+    {timestamps:true}
+);
+
+const User = mongoose.model("User", UserSchema);
+// export default User;
+
+module.exports = User
